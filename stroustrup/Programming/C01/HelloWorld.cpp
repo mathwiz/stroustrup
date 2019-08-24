af832559e9021953d993f0759c86e9949dd9f6bb
Yohan Lee <yohanlee@yahoo.com>
2019-08-23 21:22:09 -0400

    Hello World

---
+++ stroustrup/Programming/C01/HelloWorld.cpp
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.cpp
+//  stroustrup
+//
+//  Created by Yohan Lee on 8/23/19.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
