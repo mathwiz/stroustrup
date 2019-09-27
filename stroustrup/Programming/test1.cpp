5c10c0a7a14f463ffd7be14f49d9e9f8d5cc1e54
Yohan Lee <yohanlee@yahoo.com>
2019-09-26 23:22:49 -0400

    tester

---
+++ stroustrup/Programming/test1.cpp
@@ -0,0 +1,16 @@
+//
+//  test1.cpp
+//  stroustrup
+//
+//  Created by Yohan Lee on 9/26/19.
+//
+
+#include "std_lib_facilities.h"
+
+using std::cout;
+
+int main()
+{
+    std::cout << "Test finished" << std::endl;
+    return 0;
+}
